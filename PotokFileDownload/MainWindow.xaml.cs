b7ee4520bcc43b4ee7bfe74b76bc4b721fbf87e5
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-04 21:40:24 +0600

    Add project files.

---
+++ PotokFileDownload/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace PotokFileDownload
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileDownload(object sender, RoutedEventArgs e)
+        {            
+            Thread thread = new Thread(FileLoading);
+            thread.IsBackground = true;
+            thread.Priority = ThreadPriority.Normal;            
+            thread.Start();
+        }
+        private void FileLoading(object url) {
+            WebClient webClient = new WebClient();
+            var res = webClient.DownloadString(url.ToString());
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal { in });
+        }
+    }
+}
